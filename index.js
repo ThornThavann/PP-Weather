dba884721195a8b9f2d66783e50171e8c4744869
ThornThavann <thavann.thorn@institute.pse.ngo>
2024-05-04 09:59:23 +0700

    button use redirect

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function login() {
+    var email = document.getElementById("email").value;
+  
+    if (email.trim() === "") {
+      alert("Please enter email.");
+      return;
+    }
+  
+    let emailResult = checkEmail(email);
+  
+    if (emailResult) {
+      alert("Valid Email!");
+      saveEmailToLocalStorage(email);
+    } else {
+      alert("Please enter your email again ");
+    }
+  }
+  
+  function checkEmail(email) {
+    if (email.includes("@") && email.length >= 8) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  function saveEmailToLocalStorage(email) {
+      localStorage.setItem('loggedInEmail', email);
+      window.location.href = "../page/home.html";
+  }
